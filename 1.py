ed33784c7803c1f4c4257aa1876607d609e0828a
Perlita <160614177+PEPERJHGFD@users.noreply.github.com>
2024-02-21 19:44:46 -0600

    Create 1.py
    De una cadena imprime las letras impares e pares

---
+++ 1.py
@@ -0,0 +1,10 @@
+"""
+Perla Pérez Romero
+TI21
+"""
+cadena = "dejah@email.com"
+for i in range(0,len(cadena),2):
+  print(cadena[i])
+print("====:")
+for i in range(1,len(cadena),2):
+  print(cadena[i])
